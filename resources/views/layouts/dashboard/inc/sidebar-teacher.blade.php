73fb4590809dca1da06ccb7e4f7297e86782ebba
cuytamvan <rizkiwahyudi540@gmail.com>
2021-11-27 15:40:14 +0700

    update sidebar

--- resources/views/layouts/dashboard/inc/sidebar-teacher.blade.php
+++ resources/views/layouts/dashboard/inc/sidebar-teacher.blade.php
@@ -1,12 +1,12 @@
 <ul class="nav">
-  <li>
-    <a href="#!">
+  <li class="{{ Route::is('teacher.dashboard') ? 'active' : '' }}">
+    <a href="{{ route('teacher.dashboard') }}">
       <i class="nc-icon nc-bank"></i>
       <p>Dashboard</p>
     </a>
   </li>
-  <li>
-    <a href="#!">
+  <li class="{{ Route::is('teacher.scores.*') ? 'active' : '' }}">
+    <a href="{{ route('teacher.scores.index') }}">
       <i class="nc-icon nc-diamond"></i>
       <p>Nilai</p>
     </a>
